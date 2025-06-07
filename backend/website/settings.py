7d7faa70bdc0d629529331814f8aca2ad97239dd
RTR-ABINESH <abinesh.m0024@gmai.com>
2025-06-07 23:12:12 +0530

    set up mysql with djongo

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'website_db',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
